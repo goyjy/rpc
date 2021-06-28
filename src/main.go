bd54b9566a2a9e3ec2f88fbf0ea82ee50a63e65f
yzq <944207160@qq.com>
2021-06-28 23:18:14 +0800

    rpc2 : 删除代码

--- src/main.go
+++ src/main.go
@@ -1,23 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	f := Sequence()
 
-	fmt.Println(f())
-	fmt.Println(f())
-
-	f = Sequence()
-	fmt.Println(f())
-	fmt.Println(f())
-}
-
-func Sequence() func() int {
-	x := 0
-	return func() int {
-		x++
-		return x*x
-	}
 }
 
